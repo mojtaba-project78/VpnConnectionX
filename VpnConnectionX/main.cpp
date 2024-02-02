1459ad504c723bee908e4a74f9955570b2b3e2a3
Mojtaba <mojtaba.business.ceo@gmail.com>
2024-02-02 21:53:06 +0330

    Update main.cpp

--- VpnConnectionX/main.cpp
+++ VpnConnectionX/main.cpp
@@ -5,68 +5,68 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);
+	QGuiApplication app(argc, argv);
 
-    // ### register classes here
+	// ### register classes here
 
-    {
-        //==============================================================================
+	{
+		//==============================================================================
 		qmlRegisterSingletonType<XApp>("XApp.uri", 1, 0, "Api_app", [&](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
-            Q_UNUSED(engine)
-            Q_UNUSED(scriptEngine)
-            return m_app.get();
-        });
-        LOG(QString("Datatype { %1 } Successfully Registered.").arg("XApp"));
+			Q_UNUSED(engine)
+			Q_UNUSED(scriptEngine)
+			return m_app.get();
+		});
+		LOG(QString("Datatype { %1 } Successfully Registered.").arg("XApp"));
 
-        //==============================================================================
+		//==============================================================================
 		qmlRegisterSingletonType<XWindowManagement>("XWindowManagement.uri", 1, 0, "Api_windows", [&](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
-            Q_UNUSED(engine)
-            Q_UNUSED(scriptEngine)
-            return m_window.get();
-        });
-        LOG(QString("Datatype { %1 } Successfully Registered.").arg("XWindowManagement"));
+			Q_UNUSED(engine)
+			Q_UNUSED(scriptEngine)
+			return m_window.get();
+		});
+		LOG(QString("Datatype { %1 } Successfully Registered.").arg("XWindowManagement"));
 
-        //==============================================================================
-        qmlRegisterSingletonType<XAppConfiguration>("XAppConfiguration.uri", 1, 0, "Api_config", [&](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
-            Q_UNUSED(engine)
-            Q_UNUSED(scriptEngine)
-            return m_config.get();
-        });
-        LOG(QString("Datatype { %1 } Successfully Registered.").arg("XAppConfiguration"));
+		//==============================================================================
+		qmlRegisterSingletonType<XAppConfiguration>("XAppConfiguration.uri", 1, 0, "Api_config", [&](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject * {
+			Q_UNUSED(engine)
+			Q_UNUSED(scriptEngine)
+			return m_config.get();
+		});
+		LOG(QString("Datatype { %1 } Successfully Registered.").arg("XAppConfiguration"));
 
-        //==============================================================================
-    }
+		//==============================================================================
+	}
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+	QQmlApplicationEngine engine;
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+					 &app, [url](QObject *obj, const QUrl &objUrl) {
+		if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+	}, Qt::QueuedConnection);
+	engine.load(url);
 
-    m_object.InitializingObject(engine.rootObjects().first());
+	m_object.InitializingObject(engine.rootObjects().first());
 
-    m_file.initializing();
+	m_file.initializing();
 
-    m_sql.makeFile(false);
-    m_sql.initializing();
+	m_sql.makeFile(false);
+	m_sql.initializing();
 
-    m_config->initializing();
+	m_config->initializing();
 
-    m_log.makeFile(false);
-    m_log.initializing();
+	m_log.makeFile(false);
+	m_log.initializing();
 
 	m_window->openWindow(XData::Windows::WN_STARTUP);
 
-    int r = app.exec();
-    //==============================================================================
-    m_app = nullptr;
-    m_config = nullptr;
-    m_window = nullptr;
+	int r = app.exec();
+	//==============================================================================
+	m_app = nullptr;
+	m_config = nullptr;
+	m_window = nullptr;
 
-    return r;
+	return r;
 }
